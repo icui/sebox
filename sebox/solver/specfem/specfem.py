f851a643ae575c10e30e2593258bccfde8bbf272
Congyue Cui <ccui@princeton.edu>
2021-09-25 15:12:32 -0400

    update

--- sebox/solver/specfem/specfem.py
+++ sebox/solver/specfem/specfem.py
@@ -7,52 +7,53 @@ from .utils import getsize, probe_mesher, probe_solver
 if tp.TYPE_CHECKING:
     from sebox.solver import Forward, Mesh
 
-    class Par_file(tp.TypedDict, total=False):
-        """DATA/Par_file in specfem."""
-        # 1 for forward simulation, 3 for adjoint simulation
-        SIMULATION_TYPE: int
 
-        # save forward wavefield
-        SAVE_FORWARD: bool
+class Par_file(tp.TypedDict, total=False):
+    """DATA/Par_file in specfem."""
+    # 1 for forward simulation, 3 for adjoint simulation
+    SIMULATION_TYPE: int
 
-        # use monochromatic source time function
-        USE_MONOCHROMATIC_CMT_SOURCE: bool
+    # save forward wavefield
+    SAVE_FORWARD: bool
 
-        # simulation duration
-        RECORD_LENGTH_IN_MINUTES: float
+    # use monochromatic source time function
+    USE_MONOCHROMATIC_CMT_SOURCE: bool
 
-        # model name
-        MODEL: str
+    # simulation duration
+    RECORD_LENGTH_IN_MINUTES: float
 
-        # use high order time scheme
-        USE_LDDRK: bool
+    # model name
+    MODEL: str
 
-        # number of processors in XI direction
-        NPROC_XI: int
+    # use high order time scheme
+    USE_LDDRK: bool
 
-        # number of processors in ETA direction
-        NPROC_ETA: int
+    # number of processors in XI direction
+    NPROC_XI: int
 
-        # number of chunks
-        NCHUNKS: int
+    # number of processors in ETA direction
+    NPROC_ETA: int
 
-        # compute steady state kernel for source encoded FWI
-        STEADY_STATE_KERNEL: bool
+    # number of chunks
+    NCHUNKS: int
 
-        # steady state duration for source encoded FWI
-        STEADY_STATE_LENGTH_IN_MINUTES: float
+    # compute steady state kernel for source encoded FWI
+    STEADY_STATE_KERNEL: bool
 
-        # sponge absorbing boundary
-        ABSORB_USING_GLOBAL_SPONGE: bool
+    # steady state duration for source encoded FWI
+    STEADY_STATE_LENGTH_IN_MINUTES: float
 
-        # center latitude of sponge
-        SPONGE_LATITUDE_IN_DEGREES: float
+    # sponge absorbing boundary
+    ABSORB_USING_GLOBAL_SPONGE: bool
 
-        # center longitude of sponge
-        SPONGE_LONGITUDE_IN_DEGREES: float
+    # center latitude of sponge
+    SPONGE_LATITUDE_IN_DEGREES: float
 
-        # radius of the sponge
-        SPONGE_RADIUS_IN_DEGREES: float
+    # center longitude of sponge
+    SPONGE_LONGITUDE_IN_DEGREES: float
+
+    # radius of the sponge
+    SPONGE_RADIUS_IN_DEGREES: float
 
 
 async def _xspecfem(ws: Workspace):
