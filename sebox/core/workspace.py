5d2cf93d93ae41fa6e653c199bd183da09df087b
Congyue Cui <ccui@princeton.edu>
2021-09-24 21:55:37 -0400

    update

--- sebox/core/workspace.py
+++ sebox/core/workspace.py
@@ -56,7 +56,10 @@ class Workspace(Directory):
     @property
     def task(self) -> Task:
         """Workspace task (defined as property to avoid inheriting from parent)."""
-        return self._data.get('task') or self._init.get('task')
+        if 'task' in self._data:
+            return self._data['task']
+        
+        return self._init.get('task')
     
     def __init__(self, cwd: str, concurrent: bool, data: dict, parent: tp.Optional[Workspace]):
         super().__init__(cwd)
