6279df1d43979a252119620cb074a5d3a98d871d
Congyue Cui <ccui@princeton.edu>
2021-09-26 13:10:09 -0400

    update

--- sebox/core/mpi.py
+++ sebox/core/mpi.py
@@ -1,5 +1,6 @@
 from sys import argv, stderr
 from traceback import format_exc
+from mpi4py.MPI import COMM_WORLD as comm
 import typing as tp
 import asyncio
 
@@ -7,21 +8,14 @@ from sebox import root
 
 
 # MPI rank and number of processes
-rank = tp.cast(int, None)
-size = tp.cast(int, None)
+rank: int = comm.Get_rank()
+size: int = comm.Get_size()
 
 # output file name of current process
-pid = tp.cast(str, None)
-
+pid = f'p{"0" * (len(str(size - 1)) - len(str(rank)))}{rank}'
 
 if __name__ == '__main__':
     try:
-        from mpi4py.MPI import COMM_WORLD as comm
-
-        rank = comm.Get_rank()
-        size = comm.Get_size()
-        pid = f'p{"0" * (len(str(size - 1)) - len(str(rank)))}{rank}'
-
         # saved function and arguments from main process
         (func, arg, arg_mpi) = root.load(f'{argv[1]}.pickle')
 
