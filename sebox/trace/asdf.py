d127c3edbfb06f52471b60eb156a8319b6680422
Congyue Cui <ccui@princeton.edu>
2021-09-25 23:40:19 -0400

    update

--- sebox/trace/asdf.py
+++ sebox/trace/asdf.py
@@ -12,8 +12,22 @@ def gather(ws: Trace):
     from pyasdf import ASDFDataSet
 
 
-def _scatter(src: str, stas: tp.List[str]):
-    print(src, stas)
+def _scatter(path: tp.Tuple[str, str], stas: tp.List[str]):
+    from pyasdf import ASDFDataSet
+    from sebox import Directory
+    from sebox.core.mpi import comm
+
+    rank, size = comm()
+
+    with ASDFDataSet(path[0], mode='r', mpi=False) as ds:
+        data = {}
+
+        for sta in stas:
+            wav = ds.waveforms[sta]
+            data[sta] = wav[wav.get_waveform_tags()[0]]
+        
+        zeros = '0' * (len(str(size - 1)) - len(str(rank)))
+        Directory(path[1]).dump(data, f'p{zeros}{rank}.pickle')
 
 
 async def scatter(ws: Trace):
@@ -21,7 +35,9 @@ async def scatter(ws: Trace):
     from pyasdf import ASDFDataSet
 
     src = ws.rel(ws.path_trace)
+    dst = ws.rel(ws.path_mpi)
+    path = (src, dst)
 
     with ASDFDataSet(src, mode='r', mpi=False) as ds:
         stas = ds.waveforms.list()
-        await ws.mpiexec(_scatter, root.task_nprocs, arg=src, arg_mpi=stas)
+        await ws.mpiexec(_scatter, root.task_nprocs, arg=path, arg_mpi=stas)
