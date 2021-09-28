687d1166ec179502b47ea4d76cf384a61e8a34ad
Congyue Cui <ccui@princeton.edu>
2021-09-28 15:26:36 -0400

    update

--- sebox/kernel/ortho/ft.py
+++ sebox/kernel/ortho/ft.py
@@ -8,38 +8,6 @@ if tp.TYPE_CHECKING:
     from numpy import ndarray
     from .typing import Kernel
 
-    class Stats(tp.TypedDict, total=False):
-        """Source encoding data dict."""
-        # length of original trace
-        npts: int
-
-        # time step of original trace
-        delta: float
-
-        # time step
-        dt: float
-
-        # total number of time steps
-        nt: int
-
-        # transient time step
-        nt_ts: int
-
-        # steady state time step
-        nt_se: int
-
-        # minimum frequency index
-        imin: int
-
-        # maximum frequency index
-        imax: int
-
-        # event name
-        event: tp.Optional[str]
-
-        # MPI trace directory
-        path: str
-
 
 def ft_syn(ws: Kernel, data: ndarray):
     from scipy.fft import fft
@@ -66,28 +34,15 @@ def ft_obs(ws: Kernel, data: ndarray):
 
 async def ft(ws: Kernel):
     # load trace parameters
-    stats: Stats = ws.load('forward/traces/stats.pickle')
-    stats.update({
-        'npts': stats['nt'],
-        'delta': stats['dt'],
-        'dt': ws.dt,
-        'nt_ts': ws.nt_ts,
-        'nt_se': ws.nt_se,
-        'imin': ws.imin,
-        'imax': ws.imax,
-        'event': ws.event,
-        'path': ws.path('forward/traces')
-    })
-
-    await ws.mpiexec(_ft, arg=stats, arg_mpi=getstations())
+    await ws.mpiexec(_ft, arg=ws, arg_mpi=getstations())
 
 
-def _ft(stats: Stats, stas: tp.List[str]):
+def _ft(ws: Kernel, stas: tp.List[str]):
     import numpy as np
     from sebox.mpi import pid
 
-    data = Directory(stats['path']).load(f'{pid}.npy')
-    output = {}
+    stats = ws.load('forward/traces/stats.pickle')
+    data = ws.load(f'forward/traces/{pid}.npy')
     
     # resample if necessary
     if not np.isclose(stats['dt'], stats['delta']):
@@ -96,14 +51,8 @@ def _ft(stats: Stats, stas: tp.List[str]):
         resample(data, int(round(stats['npts'] * stats['delta'] / stats['dt'])), axis=-1)
 
     # # FFT
-    # if event is None:
-    #     if (inv := acc.inventory) is None or station is None:
-    #         return
-        
-    #     output_nez = {}
-
-    #     for trace in stream:
-    #         output_nez[trace.stats.component] = self._ft_syn(trace.data)
+    # if stats['event'] is None:
+    #     data_ft = _ft_syn(data)
 
     #     # rotate frequencies
     #     output_rtz = rotate_frequencies(output_nez, self.fslots, params, station, inv)
