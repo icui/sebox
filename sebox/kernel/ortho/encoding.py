767f9757d238b35fa4570b92d1ae9ad84acdd86f
Congyue Cui <ccui@princeton.edu>
2021-09-28 00:50:41 -0400

    update

--- sebox/kernel/ortho/encoding.py
+++ sebox/kernel/ortho/encoding.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 import typing as tp
 
-from sebox import root
-from sebox.utils.catalog import getdir, getevents, getstations, getmeasurements
+from sebox import root, Directory
+from sebox.utils.catalog import getdir, getevents, getstations, getcomponents, getmeasurements
 
 if tp.TYPE_CHECKING:
     from numpy import ndarray
@@ -36,31 +36,31 @@ def _encode_obs(ws: Kernel, stas: tp.List[str]):
     cdir = getdir()
     event_data = cdir.load('event_data.pickle')
     encoded = np.full([len(stas), 3, ws.imax - ws.imin], np.nan, dtype=complex)
+    cmps = getcomponents()
 
     for event in getevents():
         # read event data
         data = cdir.load(f'ft_obs_p{root.task_nprocs}/{event}/{pid}.npy')
-        slots = ws.fslots[event]
+        slots = np.array(ws.fslots[event])
+        groups = slots // ws.frequency_increment
         hdur = event_data[event][-1]
         tshift = 1.5 * hdur
         
         # source time function of observed data and its frequency component
         stf = np.exp(-((t - tshift) / (hdur / 1.628)) ** 2) / np.sqrt(np.pi * (hdur / 1.628) ** 2)
         sff = _ft_obs(ws, stf)
-        pff = np.exp(2 * np.pi * 1j * freq * (ws.nt_ts * ws.dt - tshift)) / sff
+        pshift = np.exp(2 * np.pi * 1j * freq * (ws.nt_ts * ws.dt - tshift)) / sff
 
         # record frequency components
-        for idx in slots:
-            group = idx // ws.frequency_increment
-            pshift = pff[idx]
+        for i, sta in enumerate(stas):
+            m = getmeasurements(event=event, station=sta)
 
-            # phase shift due to the measurement of observed data
-            for i, sta in enumerate(stas):
-                m = getmeasurements(event=event, station=sta, group=group)
+            for j in range(3):
+                mw = np.squeeze(np.where(m[j, groups]))
 
-                if m.any():
-                    j = np.squeeze(np.where(m))
-                    encoded[i, j, idx] = data[i, j, idx] * pshift
+                if len(mw):
+                    idx = slots[mw]
+                    encoded[i, j, idx] = data[i, j, idx] * pshift[idx]
     
     ws.dump(encoded, f'{pid}.pickle', mkdir=False)
 
