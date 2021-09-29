c744e67fc690e4177b17fa4c9588f8308768d4cc
Congyue Cui <ccui@princeton.edu>
2021-09-29 16:40:16 -0400

    update

--- sebox/kernel/ortho/diff.py
+++ sebox/kernel/ortho/diff.py
@@ -36,6 +36,5 @@ async def _diff(ws: Kernel, stas: tp.List[str]):
     phase_gather = comm.allgather(phase_diff)
 
     if 'II.OBN' in stas:
-        print('@', phase_diff.shape)
-        print('@', phase_gather)
+        print('@', phase_gather[9] == phase_diff)
 
