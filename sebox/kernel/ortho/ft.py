ad456ceeb31490fc0b7eb4318dd32cbc47ab1822
Congyue Cui <ccui@princeton.edu>
2021-10-09 18:20:14 -0400

    update

--- sebox/kernel/ortho/ft.py
+++ sebox/kernel/ortho/ft.py
@@ -122,7 +122,9 @@ def mf(enc: Encoding, stas: tp.List[str], misfit_only: bool = True):
     phase_diff *= weight * enc['phase_factor']
     amp_diff *= weight * enc['amplitude_factor']
 
-    if enc.get('normalize_frequency'):
+    ###
+    if True:
+    # if enc.get('normalize_frequency'):
         omega = np.arange(enc['imin'], enc['imax']) / enc['imin']
         phase_diff /= omega
 
