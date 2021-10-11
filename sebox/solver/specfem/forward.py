7d4b6c3dd7d2dac71aae15b3c62e054bd91250a4
Congyue Cui <ccui@princeton.edu>
2021-10-10 20:50:15 -0400

    update

--- sebox/solver/specfem/forward.py
+++ sebox/solver/specfem/forward.py
@@ -40,55 +40,6 @@ def setup(node: Specfem):
     setpars(node, pars)
 
 
-def scatter(node: Specfem):
-    """Convert output seismograms with processing format."""
-    from sebox.utils.catalog import getstations
-
-    smap = {}
-    nt = None
-    
-    for i in range(getsize(node)):
-        if node.has(fname := f'OUTPUT_FILES/array_stations_node_{i:05d}.txt'):
-            lines = node.readlines(fname)
-
-            n = int(lines[0])
-
-            if nt is None:
-                nt = int(lines[1])
-                dt = float(lines[2])
-
-            for line in lines:
-                if '#' in line:
-                    idx, sta = line.split('#')[1].lstrip().split(' ')
-                    smap[sta] = i, int(idx) - 1, n
-
-    node.mkdir('traces')
-    node.add_mpi(_scatter, arg=(smap, nt), arg_mpi=getstations())
-
-
-def _scatter(arg: tp.Tuple[tp.Dict[str, tp.Tuple[int, int, int]], int], stas: tp.List[str]):
-    import numpy as np
-    from scipy.io import FortranFile
-    from sebox import root
-
-    smap, nt = arg
-    data = np.zeros([len(stas), 3, nt], dtype='float32')
-    cache = {}
-
-    for i, sta in enumerate(stas):
-        for j in range(3):
-            p, k, n = smap[sta]
-
-            if p not in cache:
-                d = FortranFile(f'OUTPUT_FILES/array_seismograms_node_{p:05d}.bin').read_reals(dtype='float32')
-                cache[p] = d.reshape([nt, n, 3]).transpose() # type: ignore
-            
-            data[i, j] = cache[p][j, k]
-    
-    root.mpi.mpidump(data, 'traces')
-
-
-
 def forward(node: Specfem):
     """Forward simulation."""
     node.add(setup)
