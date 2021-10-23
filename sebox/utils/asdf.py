5d220e5159b2eb60b79f99f92dbb15658317a2c1
Congyue Cui <ccui@princeton.edu>
2021-10-22 22:15:21 -0400

    update

--- sebox/utils/asdf.py
+++ sebox/utils/asdf.py
@@ -52,6 +52,9 @@ if tp.TYPE_CHECKING:
 def gettrace(ds: ASDFDataSet, sta: str, cmp: str) -> Trace:
     """Get trace based on station and component."""
     wav = ds.waveforms[sta]
+
+    tags = wav.get_waveform_tags()[0]
+    print(tags)
     return tp.cast('Trace', wav[wav.get_waveform_tags()[0]].select(component=cmp)[0])
 
 
