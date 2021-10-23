785eda07d24206298fa962d151f6ad3dd00629e3
Congyue Cui <ccui@princeton.edu>
2021-10-22 22:18:26 -0400

    update

--- sebox/utils/asdf.py
+++ sebox/utils/asdf.py
@@ -103,6 +103,7 @@ async def scatter(node: Scatter):
 
     with ASDFDataSet(node.path_input, mode='r', mpi=False) as ds:
         stats = tp.cast('Stats', node.stats or {})
+        print(stats)
 
         # fill stattions and components
         if 'stas' not in stats:
