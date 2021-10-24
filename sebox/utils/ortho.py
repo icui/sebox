68f48f2bce20c52a1440ce86b9d8acf7dc63b0f1
Congyue Cui <ccui@princeton.edu>
2021-10-23 22:42:55 -0400

    update

---
+++ sebox/utils/ortho.py
@@ -0,0 +1,15 @@
+from sebox import root, Node
+from .catalog import getdir
+
+
+def scatter_obs(node: Node):
+    """Generate observed frequencies."""
+    cdir = getdir()
+
+    for src in cdir.ls('raw_obs'):
+        event = src.split('.')[0]
+        dst = f'raw_p{root.task_nprocs}/{event}'
+        
+        if not cdir.has(dst):
+            node.add(('sebox.utils.asdf', 'scatter'), event, stats={'cmps': ['N', 'E', 'Z']},
+                path_input=cdir.path(f'raw_obs/{src}'), path_output=cdir.path(dst))
