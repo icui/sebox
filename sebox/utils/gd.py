c883a1e56d18d5a715cb398dbfe93484481385f2
Congyue Cui <ccui@princeton.edu>
2021-11-01 10:37:09 -0400

    update

--- sebox/utils/gd.py
+++ sebox/utils/gd.py
@@ -1,72 +1,33 @@
 if __name__ == '__main__':
     # check misfit values
     from sebox import root
+    root.restore()
 
-    try:
-        root.restore()
+    for optim in root:
+        if not optim.has('misfit.npy'):
+            continue
+        
+        print(f'Iteration {optim.iter}')
 
-        for i, optim in enumerate(root):
-            if len(optim) < 2 or optim[0].misfit_value is None:
+        steps = [0.0]
+        vals = [optim.load('misfit.npy').sum()]
+
+        for step in optim[-2]:
+            if not step.has('misfit.npy'):
                 continue
 
-            print(f'Iteration {i}')
-        
-            steps = [0.0]
-            vals = [optim[0].misfit_value]
-
-            if len(optim):
-                for step in optim[-2]:
-                    steps.append(step.step)
-                    vals.append(step[1].misfit_value)
+            steps.append(step.load('step.pickle'))
+            vals.append(step.load('misfit.npy').sum())
             
-            for step, val in zip(steps, vals):
-                if val is None:
-                    break
-                
-                entry = f' {step:.3e}: {val:.3e}'
-
-                if optim.done and val == min(vals):
-                    entry += ' *'
-                
-                print(entry)
-            
-            print()
-    
-    except:
-        def getmf(d, pre=False):
-            mf = 0.0
-
-            for j in range(len(d.ls())):
-                if d.has(kl := f'kl_{j:02d}'):
-                    if d.has(m := f'{kl}/phase_mf.npy'):
-                        mf += d.load(m).sum()
-
-                    if d.has(m := f'{kl}/amp_mf.npy'):
-                        mf += d.load(m).sum()
-                    
-                else:
-                    break
-
-            if mf > 0.0:
-                if pre:
-                    print(d.path())
-
-                print(f' {mf:.3e}')
-                return True
-            
-            return False
-
-
-        for i in range(len(root.ls())):
-            d_i = root.subdir(f'iter_{i:02d}')
-
-            if getmf(d_i, True):
-                for k in range(len(d_i.ls())):
-                    if not getmf(d_i.subdir(f'step_{k:02d}')):
-                        break
-            
-            else:
+        for step, val in zip(steps, vals):
+            if val is None:
                 break
             
-            print()
+            entry = f' {step:.3e}: {val:.3e}'
 
+            if optim.done and val == min(vals):
+                entry += ' *'
+            
+            print(entry)
+        
+        print()
