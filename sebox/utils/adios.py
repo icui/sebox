dc3bb7d2e2e7855d4dc679d14570ed4fc8bc0bfd
Congyue Cui <ccui@princeton.edu>
2022-01-19 19:55:48 -0500

    update

--- sebox/utils/adios.py
+++ sebox/utils/adios.py
@@ -19,7 +19,7 @@ def xsum(node: Node, mask: bool):
     _adios(node, f'xsum_kernels path.txt kernels_raw.bp')
 
     if mask:
-        _adios(node, f'xsrc_mask kernels_raw.bp {getdir().path("source_mask")} kernels_masked.bp')
+        _adios(node, f'xsrc_mask kernels_raw.bp DATABASES_MPI/solver_data.bp {getdir().path("source_mask")} kernels_masked.bp {node.layer_mask or 0}')
 
 
 def xmerge(node: Node, precond: float):
