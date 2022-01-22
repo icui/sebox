28d65684526eb6fef1e6639e1cb6602c08818951
Congyue Cui <ccui@princeton.edu>
2022-01-22 00:44:13 -0500

    update

--- sebox/solver/specfem/__init__.py
+++ sebox/solver/specfem/__init__.py
@@ -1,6 +1,6 @@
 from .mesh import mesh
 from .adjoint import adjoint
 from .forward import forward as main, align
-from .postprocess import postprocess
+from .postprocess import postprocess, smooth
 
-__all__ = ['main', 'align', 'mesh', 'adjoint', 'postprocess']
+__all__ = ['main', 'align', 'mesh', 'adjoint', 'postprocess', 'smooth']
