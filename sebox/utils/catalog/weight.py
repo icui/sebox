9d62d46f2af39445f03bd83ca588456a20b65273
Congyue Cui <ccui@princeton.edu>
2022-03-05 17:23:29 -0500

    update

--- sebox/utils/catalog/weight.py
+++ sebox/utils/catalog/weight.py
@@ -1,8 +1,26 @@
+import numpy as np
 from nnodes import Node
 
 
 def create_weighting(node: Node):
     """Computes weightings."""
+    from sebox import catalog
+
+    node.add_mpi(_weight, arg_mpi = list(range(-1, len(catalog.events))))
+
+
+def _weight(idx: int):
+    from sebox import catalog
+
+    if idx < 0:
+        # event weighting
+        locs = catalog.event_data[: 2]
+    
+    else:
+        # station weighting
+        locs = catalog.station_data[idx][: 2]
+    
+    print(locs.shape, np.count_nonzero(locs))
 
 
 def weight_events(node: Node):
