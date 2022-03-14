ecc3086772c71fbc902b2cf230de39de6f5f39c5
Congyue Cui <ccui@princeton.edu>
2022-03-14 14:12:51 -0400

    update

--- sebox/utils/catalog/traces.py
+++ sebox/utils/catalog/traces.py
@@ -11,63 +11,63 @@ def download_events(node):
 def download_traces(node):
     """Download observed data."""
     for event in node.ls('events'):
-        node.add(download_trace, event=event, name=event)
+        node.add(download_trace, event=event, name=event, cwd=f'downloads/{event}')
 
 
 def download_trace(node):
     """Download observed data of an event."""
-    node.mkdir(f'downloads/{node.event}/mseed')
-    node.mkdir(f'downloads/{node.event}/xml')
-    node.add(f'python -c "from sebox.utils.catalog.traces import request_data; request_data(\'{node.event}\')"', name='download')
-    node.add(f'python -c "from sebox.utils.catalog.traces import convert_h5; convert_h5(\'{node.event}\')"', name='convert', cwd=node.path())
+    node.add(request_data)
+    node.add(convert_h5)
 
 
-def request_data(event: str):
+def request_data(node):
     from sebox import catalog
     from obspy import read_events
     from obspy.clients.fdsn.mass_downloader import GlobalDomain, Restrictions, MassDownloader
 
-    evt = read_events(f'events/{event}')[0]
+    event = read_events(f'events/{node.event}')[0]
+    node.mkdir('mseed')
+    node.mkdir('xml')
+
     gap = catalog.download['gap']
-    eventtime = evt.preferred_origin().time
+    eventtime = event.preferred_origin().time
     starttime = eventtime - gap * 60
     endtime = eventtime + (catalog.duration + gap) * 60
 
     rst = Restrictions(starttime=starttime, endtime=endtime, **catalog.download['restrictions'])
     mdl = MassDownloader()
-    mdl.download(GlobalDomain(), rst, mseed_storage=f'downloads/{event}/mseed', stationxml_storage=f'downloads/{event}/xml')
+    mdl.download(GlobalDomain(), rst, mseed_storage=node.path('mseed'), stationxml_storage=node.path('xml'))
 
 
-def convert_h5(event: str):
+def convert_h5(node):
     from traceback import format_exc
     from pyasdf import ASDFDataSet
     from obspy import read, read_events
-    from nnodes import root
     from .index import format_station
 
-    with ASDFDataSet(f'{event}.h5', mode='w', mpi=False, compression=None) as ds:
+    with ASDFDataSet(node.path(f'{node.event}.h5'), mode='w', mpi=False, compression=None) as ds:
         try:
-            ds.add_quakeml(read_events((f'../../events/{event}')))
+            ds.add_quakeml(read_events((f'events/{node.event}')))
         
         except Exception:
-            root.write(format_exc(), 'error.log', 'a')
+            node.write(format_exc(), 'error.log', 'a')
 
         stations = set()
         station_lines = ''
 
-        for src in root.ls(f'mseed'):
+        for src in node.ls(f'mseed'):
             station = '.'.join(src.split('.')[:2])
             stations.add(station)
 
             try:
-                ds.add_waveforms(read(f'mseed/{src}'), 'raw_obs')
+                ds.add_waveforms(read(node.path(f'mseed/{src}')), 'raw_obs')
 
             except Exception:
-                root.write(format_exc(), 'error.log', 'a')
+                node.write(format_exc(), 'error.log', 'a')
         
         for station in stations:
             try:
-                ds.add_stationxml(f'xml/{station}.xml')
+                ds.add_stationxml(node.path(f'xml/{station}.xml'))
                 sta = ds.waveforms[station].StationXML.networks[0].stations[0] # type: ignore
                 ll = station.split('.')
                 ll.reverse()
@@ -75,9 +75,9 @@ def convert_h5(event: str):
                 station_lines += format_station(ll)
             
             except Exception:
-                root.write(format_exc(), 'error.log', 'a')
+                node.write(format_exc(), 'error.log', 'a')
         
-        root.write(station_lines, f'STATIONS.{event}')
+        node.write(station_lines, f'STATIONS.{node.event}')
 
 
 def compute_synthetic(node):
