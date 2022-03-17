5676d9c87ae75905b85517675b97bcd18752ed28
Congyue Cui <ccui@princeton.edu>
2022-03-17 01:30:17 -0400

    update

--- sebox/utils/catalog/download.py
+++ sebox/utils/catalog/download.py
@@ -17,7 +17,7 @@ def download_traces(node):
 def download_trace(node):
     """Download observed data of an event."""
     node.ln('../../catalog.toml')
-    node.add(f'python -c "from sebox.utils.catalog.download import request_data; request_data(\'{node.event}\')"', name='request_data')
+    # node.add(f'python -c "from sebox.utils.catalog.download import request_data; request_data(\'{node.event}\')"', name='request_data')
     node.add(f'python -c "from sebox.utils.catalog.download import convert_h5; convert_h5(\'{node.event}\')"', name='convert_h5')
 
 
