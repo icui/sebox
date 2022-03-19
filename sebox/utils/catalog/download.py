0002c510499821fcd7f88c07044b2f7fc29aad15
Congyue Cui <ccui@princeton.edu>
2022-03-18 21:30:18 -0400

    update

--- sebox/utils/catalog/download.py
+++ sebox/utils/catalog/download.py
@@ -17,7 +17,7 @@ def download_traces(node):
 def download_trace(node):
     """Download observed data of an event."""
     node.ln('../../catalog.toml')
-    node.add_mpi(request_data, arg=node.event, use_multiprocessing=True)
+    # node.add_mpi(request_data, arg=node.event, use_multiprocessing=True)
     node.add_mpi(convert_h5, arg=node.event, use_multiprocessing=True)
 
 
