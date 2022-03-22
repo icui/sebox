af52788415ae3f61b628b5a5affca413b4e6540e
Congyue Cui <ccui@princeton.edu>
2022-03-22 00:32:42 -0400

    update

--- sebox/utils/catalog/blend.py
+++ sebox/utils/catalog/blend.py
@@ -19,22 +19,26 @@ def blend_event(node):
     obs = ds1.waveforms[sta].proc_obs[2]
     syn = ds2.waveforms[sta].proc_syn[2]
 
-    _blend(obs, syn)
+    _blend(event, obs, syn)
 
 
 def blend_event_(node):
+    from functools import partial
     from asdfy import ASDFProcessor
 
     event = node.event
     node.mkdir(f'blend/{event}')
     
-    ap = ASDFProcessor((f'proc_obs/{event}.h5', f'proc_syn/{event}.h5'), f'blend_obs/{event}.h5', _blend, output_tag='blend_obs')
+    ap = ASDFProcessor((f'proc_obs/{event}.h5', f'proc_syn/{event}.h5'), f'blend_obs/{event}.h5',
+        partial(_blend, event), output_tag='blend_obs')
     node.add_mpi(ap.run, node.np, name=f'blend_{event}')
 
 
-def _blend(obs, syn):
+def _blend(event, obs, syn):
     from pyflex import Config, select_windows
     from sebox import catalog
 
     config = Config(min_period=catalog.period_min, max_period=catalog.period_max)
-    select_windows(obs, syn, config, plot=True)
+    win = select_windows(obs, syn, config, plot=True, plot_filename=f'blend/{event}/window.png')
+
+    print(win)
