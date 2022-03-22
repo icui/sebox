065bc491d78ea4c38823093f350cb4f796b1a6ee
Congyue Cui <ccui@princeton.edu>
2022-03-21 22:43:29 -0400

    blend

---
+++ sebox/utils/catalog/blend.py
@@ -0,0 +1,30 @@
+def blend(node):
+    node.mkdir('blend_obs')
+
+    for event in node.ls('events'):
+        if node.has(f'proc_obs/{event}.h5') and node.has(f'proc_syn/{event}.h5'):
+            node.add(blend_event, event=event)
+            break
+
+
+def blend_event(node):
+    from pyasdf import ASDFDataSet
+
+    event = node.event
+    
+    ds1 = ASDFDataSet(f'proc_obs/{event}.h5')
+    ds2 = ASDFDataSet(f'proc_syn/{event}.h5')
+
+
+def blend_event_(node):
+    from asdfy import ASDFProcessor
+
+    event = node.event
+    node.mkdir(f'blend/{event}')
+    
+    ap = ASDFProcessor((f'proc_obs/{event}.h5', f'proc_syn/{event}.h5'), f'blend_obs/{event}.h5', _blend, output_tag='blend_obs')
+    node.add_mpi(ap.run, node.np, name=f'blend_{event}')
+
+
+def _blend(obs, syn):
+    from pyflex import Config, select_windows
