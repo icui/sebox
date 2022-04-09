2489d1e1fe01a42d012c4dc94257383c9bdfa66c
Congyue Cui <ccui@princeton.edu>
2022-04-09 17:43:21 -0400

    update

--- sebox/catalog/download.py
+++ sebox/catalog/download.py
@@ -124,8 +124,8 @@ def _convert_bp(stas, event):
     with ASDFDataSet(f'raw_obs/{event}.h5', mode='r', mpi=False) as obs_h5, \
         ASDFDataSet(f'raw_syn/{event}.h5', mode='r', mpi=False) as syn_h5, \
         adios2.open(f'bp_obs/{event}.bp', 'w', root.mpi.comm) as bp:
-        bp.write('event', event)
-        bp.write('stations', syn_h5.waveforms.list(), end_step=True)
+        lines = root.readlines(f'events/{event}')[2:13]
+        bp.write(event, np.array([str(l.split()[-1]) for l in lines]), count=11)
         
         # print('@', event)
         # bp.set('eventname', event)
