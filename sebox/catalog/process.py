e7d88c1223e6f5cffdcc72588a457657cc0555f6
Congyue Cui <ccui@princeton.edu>
2022-04-10 23:32:01 -0400

    update

--- sebox/catalog/process.py
+++ sebox/catalog/process.py
@@ -19,6 +19,8 @@ def process_synthetic(node):
     for event in node.ls('events'):
         node.add(process_event, mode='syn', event=event, name=event,
             src=f'raw_syn/{event}.bp', dst=f'proc_syn/{event}.bp')
+        
+        break
 
 
 def process_event(node):
