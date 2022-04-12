b4f65ace0ed3c374401df9447b918eaa7b01d6dd
Congyue Cui <ccui@princeton.edu>
2022-04-12 11:55:51 -0400

    update

--- sebox/catalog/download.py
+++ sebox/catalog/download.py
@@ -106,7 +106,7 @@ def convert_h5(arg):
 
 def convert_bp(node):
     events = node.ls('events')
-    node.add_mpi(_convert_bp, 131, args=('obs',), mpiarg=events)
+    node.add_mpi(_convert_bp, len(events), args=('obs',), mpiarg=events)
 
 
 def convert_bp_(node):
@@ -137,6 +137,7 @@ def _convert_bp(event, mode):
         bp.write(read_events(f'events/{event}'))
         invs = root.load(f'inventories/{event}.pickle')
         stas = h5.waveforms.list()
+        nw = 0
 
         for sta, inv in invs.items():
             if sta not in stas or tag not in h5.waveforms[sta].get_waveform_tags():
@@ -153,14 +154,16 @@ def _convert_bp(event, mode):
                 if len(traces):
                     print('@', sta)
                     bp.write(Stream(traces))
-                    bp.write(inv)
+                    bp.write(inv,end_step=nw%20==0)
+                    nw += 1
                 
                 else:
                     print('>', event, sta)
             
             else:
                 bp.write(h5.waveforms[sta][tag])
-                bp.write(inv)
+                bp.write(inv,end_step=nw%20==0)
+                nw += 1
 
 
 def __convert_bp_(stas, event, mode):
